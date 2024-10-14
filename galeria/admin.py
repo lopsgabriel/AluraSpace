1a0cee3bb3114834dedd1ce63193710cf32d4d60
Gabriel Lopes <souzsgabriel12@gmail.com>
2024-10-14 20:27:57 -0300

    continuação

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "descricao", "publicada")
+    list_display_links = ("id", "nome", "legenda", "descricao")
+    list_editable = ("publicada", )
+    search_fields = ("nome", )
+    list_filter = ("categoria", )
+    list_per_page = 10
+    
+
+
+
+admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Fotografia(models.Model):
+
+    opcoes_categoria = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALAXIA', 'Galaxia'),
+        ('PLANETA', 'Planeta'),
+        ('ASTRO', 'Astro')
+    ]
+
+    nome = models.CharField(max_length = 100, null = False, blank = False)
+    legenda =  models.CharField(max_length=200, null = False, blank = False)
+    categoria = models.CharField(max_length=70, choices=opcoes_categoria, default='')
+    descricao = models.TextField(null = False, blank = False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True, null=True)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    
+
+    def __str__(self):
+        return f'Fotografia  [nome = {self.nome}]'
+
+
+
